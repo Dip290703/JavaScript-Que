8602d36dccd8b05d5943f1bf852181869dafe83a
Dip290703 <dipanshu6233@gmail.com>
2025-06-05 10:32:37 +0530

    Js question

---
+++ Day-1/index.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+
+
+// This is a simple JavaScript program that prints "Hello, World!" to the console.
+
+
+function sum (a,b) {
+    const result = a + b;
+    console.log(`The sum of ${a} and ${b} is ${result}`);
+}
+
+sum(5, 10);
+sum(20, 30);
+// This function takes two numbers as arguments and prints their sum to the console.
