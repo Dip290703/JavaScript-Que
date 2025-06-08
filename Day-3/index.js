35b391740ff6813aa1620f9e89cfd4a16700a510
Dip290703 <dipanshu6233@gmail.com>
2025-06-08 12:33:03 +0530

    JS practice

--- Day-3/index.js
+++ Day-3/index.js
@@ -1,1 +1,14 @@
 console.log("Day 3: JavaScript Basics");
+function factorial(num){
+    if(num<0 || typeof(num) !== "Number"){
+       alert('number should be psoitve and number')
+        
+    }
+for(let i = num -1 ;i >0 ;i--){
+    num = num * i
+}
+return num
+}
+console.log(factorial(5)); // Output:
+console.log(factorial("hiii"));
+
