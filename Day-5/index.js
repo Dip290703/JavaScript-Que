40b1046f092deb1926e97a865a4dad10fa7faaf5
Dip290703 <dipanshu6233@gmail.com>
2025-06-09 22:23:00 +0530

    js practice

--- Day-5/index.js
+++ Day-5/index.js
@@ -1,1 +1,12 @@
 console.log("Day-5 : JavaScript Question");
+// multiplication
+
+function table(num){
+    let result = 1
+    for(let i = 1; i <= 10 ; i++){
+    
+     console.log( ` table of ${num} * ${i} : ${num* i} `);
+     
+    }
+}
+table(3)
