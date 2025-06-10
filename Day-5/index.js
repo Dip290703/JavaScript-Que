601ef1d2d6c3e8c989876a07505d72517e329be9
Dip290703 <dipanshu6233@gmail.com>
2025-06-10 20:28:46 +0530

    js Question

--- Day-5/index.js
+++ Day-5/index.js
@@ -32,3 +32,22 @@ function biggestNumber () {
 }
 biggestNumber()
 
+function smallestNumber () {
+    let lengthOfArray = parseInt(prompt("Enter length of array"))
+    let numbers = []
+    for (let i = 0; i < lengthOfArray; i ++ ){
+        let number = parseInt(prompt("Enter numbers"))
+        numbers.push(number)
+    }
+    console.log(numbers);
+    
+    let smallestNumber = numbers[0]
+    for(let i =1 ; i < numbers.length; i ++){
+        if(smallestNumber > numbers[i]){
+            smallestNumber = numbers[i]
+        }
+    }
+    console.log("samllest number", smallestNumber);
+    
+}
+smallestNumber()
