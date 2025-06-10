c01f4c94a1a5fcb727034c28e21803576ce43012
Dip290703 <dipanshu6233@gmail.com>
2025-06-10 20:27:46 +0530

    js Question

--- Day-5/index.js
+++ Day-5/index.js
@@ -10,3 +10,25 @@ function table(num){
     }
 }
 table(3)
+
+
+function biggestNumber () {
+    let lengthOfArray = parseInt(prompt("Enter length of array"))
+    let numbers = []
+    for (let i = 0; i < lengthOfArray; i ++ ){
+        let number = parseInt(prompt("Enter numbers"))
+        numbers.push(number)
+    }
+    console.log(numbers);
+    
+    let biggestNumber = numbers[0]
+    for(let i =1 ; i < numbers.length; i ++){
+        if(biggestNumber < numbers[i]){
+            biggestNumber = numbers[i]
+        }
+    }
+    console.log("biggest number", biggestNumber);
+    
+}
+biggestNumber()
+
