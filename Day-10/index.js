43f195e26c357ba18bd964aa794be6bc66fa1c98
Dip290703 <dipanshu6233@gmail.com>
2025-06-14 09:24:19 +0530

    js Question

--- Day-10/index.js
+++ Day-10/index.js
@@ -7,3 +7,18 @@ return result/num.length
 
 }
 console.log(average([1,3,34]));
+
+function calAverage (numArray){
+    if(Array.isArray(numArray) !== true ){
+        console.log("Invalid input");
+        
+    } 
+    let sum = 0
+
+    for (let i = 0 ; i < numArray.length ; i++){
+        sum += numArray[i]
+    }
+    let average = sum / numArray.length
+    return average
+}
+console.log(calAverage([1,23,4,5,6]));
