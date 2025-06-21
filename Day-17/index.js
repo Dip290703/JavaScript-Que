d3550f09ecc116d888bcf19f26d22c84f676e5d0
Dip290703 <dipanshu6233@gmail.com>
2025-06-21 11:36:46 +0530

    js

--- Day-17/index.js
+++ Day-17/index.js
@@ -1,1 +1,15 @@
-console.log("hiii");
+
+
+function countCharacter (input){
+    const result ={}
+    let string = input.replace(/\s/g,"").toLowerCase()
+    for(let i =0 ; i < string.length;i++){
+        if(!result[string[i]]){
+            result[string[i]] = 0
+        }
+        result[string[i]] ++
+    }
+    console.log(result);
+    
+}
+countCharacter("Dipanshu Anil Vishwakarma")
