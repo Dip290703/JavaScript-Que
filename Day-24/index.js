2dbe3a0d2c08a85e5eebd82e385b54f10f0a1e96
Dip290703 <dipanshu6233@gmail.com>
2025-06-30 19:02:34 +0530

    js

--- Day-24/index.js
+++ Day-24/index.js
@@ -1,1 +1,15 @@
 console.log("Hello");
+        
+
+
+function password (){
+   let password = prompt("Enter your password");
+   let password2 = prompt("enter pass")
+   if(password === password2){
+    console.log("password is correct")
+
+   }else{
+    console.log("password is incorrect")
+   }
+}
+password()
