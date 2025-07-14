9ace78ffe8eb8c49621462f3dec3507d71815044
Dip290703 <dipanshu6233@gmail.com>
2025-07-14 10:25:24 +0530

    context

--- Day-28/context/src/components/Login.jsx
+++ Day-28/context/src/components/Login.jsx
@@ -1,28 +1,53 @@
-import React, { useState,useContext } from 'react'
-import UserContext from '../context/UserContext'
+import { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
 
 const Login = () => {
-    const [username,setUsername] = useState("")
-    const [password , setPassword] = useState("")
-  const {setUser} = useContext(UserContext)
-    const handleSumbit = (e) => {
-    e.preventDefault()
-    setUser({username,password})
-    }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [input, setInput] = useState("");
+  const [location, setLocation] = useState("");
+  const { setUser } = useContext(UserContext);
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  const handleloction = (e) => {
+    e.preventDefault();
+    setLocation(input);
+  };
   return (
     <div>
-        <h1>Login</h1>
-        <input 
+      <h1>Login</h1>
+      <input
         value={username}
-        onChange={(e) => {setUsername(e.target.value)}}
-        type="text"  placeholder='username' />
-        <input
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+        type="text"
+        placeholder="username"
+      />
+      <input
         value={password}
-        onChange={(e) => {setPassword(e.target.value)}}
-        type="text" placeholder='password' />
-        <button onClick={handleSumbit}>Sumbit</button>
-    </div>
-  )
-}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        type="text"
+        placeholder="password"
+      />
+      <button onClick={handleSumbit}>Sumbit</button>
 
-export default Login
+      <input
+        type="text"
+        placeholder="location"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button onClick={handleloction}>location</button>
+      <h1> location{location}</h1>
+    </div>
+  );
+};
+
+export default Login;
