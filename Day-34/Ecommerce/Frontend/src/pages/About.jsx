9c39ed5daa782365b4a2b4ffa904438aa472096c
Dip290703 <dipanshu6233@gmail.com>
2025-07-24 11:29:15 +0530

    Frontend Complete

--- Day-34/Ecommerce/Frontend/src/pages/About.jsx
+++ Day-34/Ecommerce/Frontend/src/pages/About.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewesLetterBox from '../components/NewesLetterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+     <Title text1={"ABOUT"} text2={"US"}/>
+
+      </div>
+      <div className='flex my-10 flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} alt="about"  className='w-full max-w-[450px] '/>
+       <div className='flex flex-col gap-6 justify-center md:w-2/4 text-gray-600'>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero eos aspernatur quae, et esse dicta quia animi error, aperiam neque blanditiis quasi, ipsa itaque dolorem cum voluptatem praesentium minus iusto asperiores. Et, eius delectus!</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, ab nobis ipsum beatae harum vero aspernatur quam tempora voluptate amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, laudantium!.</p>
+       <b className='text-gray-800'>Our Mission</b>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quod id sequi, autem rerum quos!</p>
+       </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={"WHY"} text2={"CHOOSE"}/>
+      </div>
+      <div className='flex flex-col md:flex-row  text-sm mb-20'>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Quality Assurance:</b>
+        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse. Fuga id laboriosam quod velit.</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Convenience:</b>
+        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse. Fuga id laboriosam quod velit.</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Exceptional Customer Service:</b>
+        <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, esse. Fuga id laboriosam quod velit.</p>
+      </div>
+      </div>
+      <NewesLetterBox />
+    </div>
   )
 }
 
--- Day-34/Ecommerce/Frontend/src/pages/Login.jsx
+++ Day-34/Ecommerce/Frontend/src/pages/Login.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up');
+  const onSubmit =  async (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>Login</div>
+   <form 
+   onSubmit={onSubmit}
+   className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+    <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+      <p className='prata-regular text-3xl'>
+        {currentState}
+      </p>
+      <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+    </div>
+
+  {
+    currentState === 'Login' ? "" : (
+        <input type="text" placeholder='Name' className='w-full px-3 py-2 border border-gray-800' required />
+    )
+  }
+    <input type="email" placeholder='Email' className='w-full px-3 py-2 border border-gray-800' required />
+    <input type="password" placeholder='Password' className='w-full px-3 border py-2 border-gray-800' required />
+    <div className='w-full flex justify-between text-sm mt-[-8px]'>
+      <p className='cursor-pointer'>Forget Password?</p>
+      {
+    currentState === 'Sign Up' ? (
+      <button
+        className='text-sm text-gray-800 hover:text-gray-600'
+        onClick={() => setCurrentState('Login')}
+        type='button'
+      >
+        Already have an account? Login
+      </button>
+    ) : (
+      <button
+        className='text-sm text-gray-800 hover:text-gray-600'
+        onClick={() => setCurrentState('Sign Up')}
+        type='button'
+      >
+        Don't have an account? Sign Up
+      </button>
+    )
+  } 
+    </div>
+    <button className='w-full bg-gray-800 text-white py-2 rounded hover:bg-gray-700 transition-colors duration-300 mt-4 ' >
+      {
+        currentState === 'Login' ? ' Sign in' : 'Sign Up'
+      }
+    </button>
+   </form>
   )
 }
 
