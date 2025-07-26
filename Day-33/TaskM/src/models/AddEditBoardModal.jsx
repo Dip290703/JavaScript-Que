d2e98fb00fc68bb3032303ec4230d005c135e8b4
Dip290703 <dipanshu6233@gmail.com>
2025-07-26 22:02:52 +0530

    models

--- Day-33/TaskM/src/models/AddEditBoardModal.jsx
+++ Day-33/TaskM/src/models/AddEditBoardModal.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import cross from "../assets/icon-cross.svg"; // Assuming you have a cross icon in your assets
+import { v4 as uuid } from "uuid"; // Importing uuid for unique IDs
 
 const AddEditBoardModal = ({ setBoardModalOpen, type }) => {
+  const [name,setName]=useState('')
+  const [columns, setColumns] = useState(
+    [
+      {name:"Todo", task :[],id:uuid()},
+      {name:"Doing", task :[],id:uuid()},
+  
+    ]
+  );
+  const onChange = (id , newValue) => {
+     setColumns((prevState)=>{
+      const newState =  [...prevState]
+      const column = newState.find((col)=>col.id === id)
+      column.name = newValue
+      return newState
+     })
+  }
     //console.log(type);
     
   return (
@@ -21,10 +38,30 @@ const AddEditBoardModal = ({ setBoardModalOpen, type }) => {
         <h3 className="text-lg">
           {type === "edit" ? "Edit " : "Add New"} Board
         </h3>
-        <div className="mt-4 flex ">
-           <input type="text" placeholder="Add new board" className="text-sm border  py-2 w-full  px-2 rounded outline-none" />
-           <img src={cross} alt="" className="" />
+        <div className="mt-8 flex flex-col space-y-3">
+          <label className="text-sm dark:text-white text-gray-500">Board Name</label>
+           <input type="text" placeholder="eg. Web design" className=" bg-transparent text-sm border px-4 py-2 w-full  border-gray-600 focus:outline-[#635fc7] rounded-md outline-1 ring-0"
+           onChange={(e) => setName(e.target.value)}
+            value={name}
+            id="board-name-input"
+            />
         </div>
+     <div className="mt-8 flex flex-col space-y-3">
+      <label className="text-sm dark:text-white text-gray-500">Board Columns</label>
+      {
+       columns.map((column,index)=>(
+        <div key={index}
+        className="flex items-center w-full ">
+         <input type="text"  className=" bg-transparent flex-grow text-sm border px-4 py-2 w-full  border-gray-600 focus:outline-[#635fc7] rounded-md outline-1 ring-0"
+           value={column.name}
+           onChange={(e)=>onChange(column.id, e.target.value)}
+            />
+            <img src={cross} alt=""
+            className="cursor-pointer m-4 " />
+        </div>
+       ))
+      }
+     </div>
       </div>
     </div>
   );
