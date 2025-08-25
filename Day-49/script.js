7a41aed26fa7725d403577da3e0dcbe73ac19ec2
Dip290703 <dipanshu6233@gmail.com>
2025-08-25 11:35:48 +0530

    js question practice

--- Day-49/script.js
+++ Day-49/script.js
@@ -26,4 +26,44 @@ function largest(arr1){
 }
 console.log(largest(arr1));
 
-//
+//--------------------------------------------------------------------------//
+
+//find the all factors of a number
+let num = 20
+function factors(num){
+    let factor = []
+    for(let i = 1 ; i <= num ; i++){
+        if(num % i == 0){
+            factor.push(i)
+        }
+    }
+    return factor
+}
+console.log(factors(num));
+//--------------------------------------------------------------------------//
+
+
+//find the number of words in a string
+let str = "Hello world this is javascript"
+function countWords(str){
+    let words = str.split(" ")
+    console.log(words);
+    
+    return words.length
+}
+console.log(countWords(str));
+//--------------------------------------------------------------------------//
+// couting the number of lettte in a string without spaces
+let str1 = "Hello world this is javascript"
+function countLetters(str1){
+    let count = 0
+    for(let i = 0 ; i < str1.length ; i++){
+        if(str1[i] != " "){
+            count++
+        }
+    }
+    return count
+}
+console.log(countLetters(str1));
+
+
