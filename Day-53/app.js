b0d5c590a6a530f25ccede2474c309e979010038
Dip290703 <dipanshu6233@gmail.com>
2025-09-04 11:02:27 +0530

    module.exports and require

--- Day-53/app.js
+++ Day-53/app.js
@@ -27,8 +27,21 @@
 // console.log(globalThis === global);
 // console.log(globalThis);
 const { smallestNumber , largestNumber , isPalindrome , countWord , countLetter } = require('./function')
+//const fn = require('./function')
+//fn.smallestNumber([1,2,3,4,5,6,7,8,9,10])
 smallestNumber([1,2,3,4,5,6,7,8,9,10])
 largestNumber([1,2,3,4,5,6,7,8,9,10])
 isPalindrome("madam")
 countWord("Hello world this is a test")
 countLetter("Hello world this is a test")
+
+
+
+//  1. Installation of node
+//  2. REPL = { Read , Evaluate , Print , Loop }
+//  3. run Javascript code in node via =  node file.js
+//  4. Architecture of node 
+//  5. V8 engine = it helps to execute Js code 
+//  6. global = it is special keyword that reffers the global object which is provided by the node js
+//  7. done using node like **require("/pathName")** and **module.exports**
+//  8. if you want to use code which is present inside the another file then you have to use **module.exports** to export and **require("/pathName") **for  import  then you can code which is in another file.
