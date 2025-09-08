8599f05a8ada94bc1d083873fec6847913698ac1
Dip290703 <dipanshu6233@gmail.com>
2025-09-08 10:14:27 +0530

    practice

---
+++ Day-54/app.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Day 54 of 100 Days of Code!");
+
+// const {add } = require('./funtion.js');
+//Es modules
+//In es6 we have to use export and import keyword to share the code between two files
+//In node js we have to use module.exports and require keyword to share the code between two files
+//In es6 we have to use .js extension while importing a file
+//In node js we don't have to use .js extension while importing a file
+// import {add} from './funtion.js'
+console.log(add(5,6));
+z = "This is Day 54 of 100 Days of Code"
